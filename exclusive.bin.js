e6f8668551533cce4029a00c2c694adc457611bd
Alan Gutierrez <alan@prettyrobots.com>
2017-10-24 20:20:33 -0500

    Upgrade `destructible` to 0.17.x.

--- exclusive.bin.js
+++ exclusive.bin.js
@@ -49,5 +49,6 @@ require('arguable')(module, require('cadence')(function (async, program) {
 
     logger.info('started', {})
 
+    destructible.compassion.wait(async())
     program.ready.unlatch()
 }))
